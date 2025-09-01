94db1dea0b46d05c90d180abcad27e476f7486f9
dilen1999 <dilengeorge@gmail.com>
2025-09-01 20:00:07 +0530

    fix: add the working condition ticket services

--- src/Services/ticket-service/src/main/java/com/eventticketbooking/ticket/ticket_service/config/SwaggerConfig.java
+++ src/Services/ticket-service/src/main/java/com/eventticketbooking/ticket/ticket_service/config/SwaggerConfig.java
@@ -1,19 +1,19 @@
-// package com.eventticketbooking.ticket.ticket_service.config;
+package com.eventticketbooking.ticket.ticket_service.config;
 
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.servers.Server;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import java.util.List;
+import java.util.List;
 
-// @Configuration
-// public class SwaggerConfig {
+@Configuration
+public class SwaggerConfig {
 
-//   @Bean
-//   public OpenAPI ticketOpenAPI() {
-//     // Gateway exposes TicketService under /tickets/**
-//     return new OpenAPI()
-//         .servers(List.of(new Server().url("/tickets")));
-//   }
-// }
+  @Bean
+  public OpenAPI ticketOpenAPI() {
+    // Gateway exposes TicketService under /tickets/**
+    return new OpenAPI()
+        .servers(List.of(new Server().url("/")));
+  }
+}
