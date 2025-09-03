9e813a9cf8562802856f6560f3dcb3e70531150a
Kalangi Jayakody <thathsarakalangi@gmail.com>
2025-09-03 10:47:11 +0530

    fix:added swagger configurations

--- src/Services/ticket-service/src/main/java/com/eventticketbooking/ticket/ticket_service/config/SwaggerConfig.java
+++ src/Services/ticket-service/src/main/java/com/eventticketbooking/ticket/ticket_service/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-// package com.eventticketbooking.ticket.ticket_service.config;
+package com.eventticketbooking.ticket.ticket_service.config;
 
 // import io.swagger.v3.oas.models.OpenAPI;
 // import io.swagger.v3.oas.models.servers.Server;
@@ -17,3 +17,25 @@
 //         .servers(List.of(new Server().url("/tickets")));
 //   }
 // }
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+/**
+ * Force OpenAPI "servers" to the gateway path so Swagger UI calls the gateway,
+ * not the internal Docker hostname.
+ */
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI ticketOpenAPI() {
+    // Gateway exposes tickets at /tickets/**
+    return new OpenAPI().servers(List.of(new Server().url("/tickets")));
+  }
+}
+
