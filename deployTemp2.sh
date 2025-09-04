6a11287ff3c925dffc395d1746e6fe3410fcbaa0
Githmi Sundarasekara <githmisundarasekara@gmail.com>
2025-09-04 22:19:25 +0530

    fix: update and fix issues in k8s manifests of services and gateway

--- deployTemp2.sh
+++ deployTemp2.sh
@@ -3,37 +3,29 @@ set -e
 
 echo "Applying ConfigMaps and Secrets..."
 kubectl apply -f k8s/configmaps/common-configmap.yaml
-kubectl apply -f k8s/configmaps/userservice-configmap.yaml
-kubectl apply -f k8s/configmaps/eventservice-configmap.yaml
 kubectl apply -f k8s/configmaps/notificationservice-configmap.yaml
+kubectl apply -f k8s/configmaps/ticketservice-configmap.yaml
 kubectl apply -f k8s/configmaps/gateway-configmap.yaml
-kubectl apply -f k8s/secrets/userservice-secret.yaml
-kubectl apply -f k8s/secrets/eventservice-secret.yaml
 kubectl apply -f k8s/secrets/gateway-secret.yaml
-
-
+kubectl apply -f k8s/secrets/ticketservice-secret.yaml
 
 echo "Applying Services..."
-kubectl apply -f k8s/services/userservice-service.yaml
-kubectl apply -f k8s/services/eventservice-service.yaml
 kubectl apply -f k8s/services/notificationservice-service.yaml
-kubectl apply -f k8s/services/mongo-service.yaml
-kubectl apply -f k8s/services/postgres-service.yaml
+kubectl apply -f k8s/services/ticketservice-service.yaml
+kubectl apply -f k8s/services/ticketpostgres-service.yaml
 kubectl apply -f k8s/services/kafka-service.yaml
 kubectl apply -f k8s/services/zookeeper-service.yaml
 kubectl apply -f k8s/services/gateway-service.yaml
 
 echo "Deploying Databases..."
-kubectl apply -f k8s/deployments/databases/mongo-deployment.yaml
-kubectl apply -f k8s/deployments/databases/postgres-deployment.yaml
+kubectl apply -f k8s/deployments/databases/ticketpostgres-deployment.yaml
 
 echo "Deploying Kafka..."
 kubectl apply -f k8s/deployments/infrastructure/
 
 echo "Deploying Microservices..."
-kubectl apply -f k8s/deployments/microservices/userservice-deployment.yaml
-kubectl apply -f k8s/deployments/microservices/eventservice-deployment.yaml
 kubectl apply -f k8s/deployments/microservices/notificationservice-deployment.yaml
 kubectl apply -f k8s/deployments/microservices/gateway-deployment.yaml
+kubectl apply -f k8s/deployments/microservices/ticketservice-deployment.yaml
 
 echo "Deployment complete! Check pods with: kubectl get pods"
